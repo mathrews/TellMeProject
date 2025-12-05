e7277c42af89e0a6ce33f482d933f2c768990d06
mathrews <mateusps4gg@gmail.com>
2025-12-05 14:48:55 -0300

    Initial Commit

---
+++ TellMeProject/TellMeProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TellMeProjectApp.swift
+//  TellMeProject
+//
+//  Created by found on 05/12/25.
+//
+
+import SwiftUI
+
+@main
+struct TellMeProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
