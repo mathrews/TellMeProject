ad06d152b6000c9cd8b1912d2c3b0a337b777779
distobs <cuppotatocake@gmail.com>
2025-12-09 15:33:22 -0300

    Alicerce básico muito primitivo para o upload.

--- TellMeProject/ScreenViews/UploadView.swift
+++ TellMeProject/ScreenViews/UploadView.swift
@@ -8,13 +8,49 @@
 import SwiftUI
 import SwiftUIExtras
 
+enum Category {
+    case pintura, fotografia, texto, poema, musica, escultura, video
+};
+
 struct UploadView: View {
     @Environment(\.dismiss) var dismiss
+    @State private var titulo: String = ""
+    @State private var desc: String = ""
+    @State private var autor: String = ""
+    @State private var ano = Date()
+    @State private var tags: [String] = []
+    @State private var categ: Category? = nil
 
     var body: some View {
         VStack {
-            Text("Main Content")
+            TextField("Título", text: $titulo)
+                .textFieldStyle(.plain)
+                .padding(10)
+                .border(Color.black, width: 0.5)
+                .background(Color(UIColor.secondarySystemBackground))
+
+            TextField("Descrição", text: $desc, axis: Axis.vertical)
+                .textFieldStyle(.plain)
+                .padding(10)
+                .border(Color.black, width: 0.5)
+                .background(Color(UIColor.secondarySystemBackground))
+
+            TextField("Autor", text: $autor)
+                .textFieldStyle(.plain)
+                .padding(10)
+                .border(Color.black, width: 0.5)
+                .background(Color(UIColor.secondarySystemBackground))
+
+            HStack {
+                Text("Ano")
+                Picker("", selection: $ano) {
+                    ForEach(2000...2025, id: \.self) {
+                        Text(String($0))
+                    }
+                }
+            }
         }
+        .padding()
     }
 }
 
